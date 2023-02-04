ae410203dacbbb9bac866c9e9c525d0645aad69b
Nicolas Mitestainer <nicolas.mitestainer08@gmail.com>
2023-02-03 23:48:24 -0300

    js-perguntas-frequentes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 // Ativar menu
-const links = document.querySelectorAll(".header-menu a")
+const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
   const url = location.href;
-  const href = link.href
+  const href = link.href;
 
-  if(url.includes(href)) {
-    link.classList.add("ativo")
+  if (url.includes(href)) {
+    link.classList.add("ativo");
   }
 }
 
-links.forEach(ativarLink)
+links.forEach(ativarLink);
 
 // Ativar Itens do orçamento
 const parametros = new URLSearchParams(location.search);
@@ -18,8 +18,28 @@ const parametros = new URLSearchParams(location.search);
 function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro);
   if (elemento) {
-    elemento.checked = true
+    elemento.checked = true;
   }
 }
 
-parametros.forEach(ativarProduto)
+parametros.forEach(ativarProduto);
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa")
+  pergunta.setAttribute("aria-expanded", ativa)
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
